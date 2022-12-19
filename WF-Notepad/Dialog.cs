a899d40d9049e94d3c490e6889bba0cb79d683da
eko-develops <ekodevelops@gmail.com>
2022-12-18 20:29:56 -0800

    add dialog class

---
+++ WF-Notepad/Dialog.cs
@@ -0,0 +1,67 @@
+﻿
+
+using System.Windows.Forms;
+
+namespace WF_Notepad
+{
+    class Dialog
+    {
+        public static void SaveAs(string text)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            saveFileDialog.Title = "Save Note As..";
+            saveFileDialog.FileName = "*.txt";
+            saveFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFileDialog.RestoreDirectory = true;
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter writer = new StreamWriter(saveFileDialog.OpenFile());
+
+                writer.WriteLine(text);
+
+                writer.Dispose();
+
+                writer.Close();
+
+            }
+        }
+
+        public static void OpenFile(MainForm mainForm, RichTextBox mainText)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.Title = "Open";
+
+            openFileDialog.CheckFileExists = true;
+            openFileDialog.CheckPathExists = true;
+
+            openFileDialog.DefaultExt = "txt";
+            openFileDialog.Filter = "txt files (*.txt)|*.txt";
+            openFileDialog.FilterIndex = 2;
+            openFileDialog.RestoreDirectory = true;
+
+            openFileDialog.ReadOnlyChecked = true;
+            openFileDialog.ShowReadOnly = true;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                mainForm.Text = openFileDialog.SafeFileName;    // set the window name
+
+                // using the System.IO.File namespace to use the method to open, read, and close file
+                // could look into StreamReader as well
+                // mainText.Text = System.IO.File.ReadAllText(openFileDialog.FileName);
+
+                StreamReader reader = new StreamReader(openFileDialog.FileName);
+
+                mainText.Text = reader.ReadToEnd();
+
+                reader.Dispose();
+
+                reader.Close();
+
+            }
+        }
+    }
+}
--- WF-Notepad/Form1.cs
+++ WF-Notepad/Form1.cs
@@ -17,60 +17,13 @@ namespace WF_Notepad
 
             text = mainText.Text.ToString();
 
-            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            Dialog.SaveAs(text);
 
-            saveFileDialog.Title = "Save Note As..";
-            saveFileDialog.FileName = "*.txt";
-            saveFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
-            saveFileDialog.RestoreDirectory = true;
-
-            if (saveFileDialog.ShowDialog() == DialogResult.OK)
-            {
-                StreamWriter writer = new StreamWriter(saveFileDialog.OpenFile());
-
-                writer.WriteLine(text);
-
-                writer.Dispose();
-
-                writer.Close();
-
-            }
         }
 
         private void buttonOpen_Click(object sender, EventArgs e)
         {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-
-            openFileDialog.Title = "Open";
-
-            openFileDialog.CheckFileExists = true;
-            openFileDialog.CheckPathExists = true;
-
-            openFileDialog.DefaultExt = "txt";
-            openFileDialog.Filter = "txt files (*.txt)|*.txt";
-            openFileDialog.FilterIndex = 2;
-            openFileDialog.RestoreDirectory = true;
-
-            openFileDialog.ReadOnlyChecked = true;
-            openFileDialog.ShowReadOnly = true;
-
-            if (openFileDialog.ShowDialog() == DialogResult.OK)
-            {
-                this.Text = openFileDialog.SafeFileName;    // set the window name
-
-                // using the System.IO.File namespace to use the method to open, read, and close file
-                // could look into StreamReader as well
-                // mainText.Text = System.IO.File.ReadAllText(openFileDialog.FileName);
-
-                StreamReader reader = new StreamReader(openFileDialog.FileName);
-
-                mainText.Text = reader.ReadToEnd();
-
-                reader.Dispose();
-
-                reader.Close();
-                
-            }
+            Dialog.OpenFile(this, mainText);
         }
 
       
